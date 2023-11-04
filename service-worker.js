21d5b092118a2bf6bbf583f997c303c04d2ffea4
sidhuns <nihaalsidhu03@gmail.com>
2023-11-04 17:34:42 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,13 @@
-var Checksites = function (websiteurl){
-    let satireurl = ["theonion.com", "cracked.com" , "babylonbee.com"]
+function Checkwebsites(websiteurl){
+    let satireurl = ["theonion.com", "cracked.com" , "babylonbee.com"];
     if (websiteurl.length === 0 ) {
-        return "The website url provided is empty"
+        return "The website url provided is empty";
     }
+    let infunction = new URL(websiteurl) ;
+
+    satireurl.forEach(url , () => {
+        if (url == infunction.hostname){
+            return "This is a satire site, displays funny information"
+        }
+    } )
 }
