8dc7aa1081fee0bc02c3e845bb49740f4b1d4db8
Jack Detrick <jackdetrick6@gmail.com>
2023-11-04 18:01:06 -0400

    fixed readings

---
+++ birdReading.js
@@ -0,0 +1,10 @@
+let readingContent = document.querySelectorAll('a > h3');
+let readingList = [];
+
+readingContent.array.forEach(element => {
+    if(crumb.href){
+        readingList.push(crumb.href);
+    }
+    
+});
+
