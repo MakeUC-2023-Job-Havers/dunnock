cd6230faf1501afd1c14acc2831bf27345a8fb8c
sidhuns <nihaalsidhu03@gmail.com>
2023-11-04 17:21:04 -0400

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,6 @@
+var Checksites = function (websiteurl){
+    let satireurl = ["theonion.com", "cracked.com" , "babylonbee.com"]
+    if (websiteurl.length === 0 ) {
+        return "The website url provided is empty"
+    }
+}
