e2909e2510dc52f9017b6d8635c21b0314a07e53
Jack Detrick <jackdetrick6@gmail.com>
2023-11-04 22:13:10 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -14,6 +14,6 @@ function Checkwebsites(websiteurl){
     } )
 };
 
-// readingList.forEach(element => {
-//     Checkwebsites(element);
-// });
+readingList.forEach(element => {
+     Checkwebsites(element);
+ });
