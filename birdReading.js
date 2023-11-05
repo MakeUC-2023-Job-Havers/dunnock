3b4834f7f8515a8fabe72fdf2cafccd88faba172
Jack Detrick <jackdetrick6@gmail.com>
2023-11-04 20:58:34 -0400

    ez

---
+++ birdReading.js
@@ -0,0 +1,9 @@
+let readingContent = document.querySelectorAll('a > h3');
+let readingList = [];
+
+readingContent.forEach(element => {
+    if(element.href){
+        readingList.push(element.href);
+    }
+    
+});
